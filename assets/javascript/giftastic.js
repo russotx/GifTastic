de5357987e0583097e4de89fee9daa48470b4942
russotx <russotx@gmail.com>
2017-03-27 23:58:15 -0500

    added animation clicks

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -98,7 +98,17 @@ $(document).ready(function(){
     $('#add-gif-form').val("");
   });
 
-
+  $('#gif-display').on("click",".posted-gifs",function(){
+    var state = $(this).attr("data-state");
+    if (state==="still") {
+      $(this).attr("src",$(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+    }
+  });
 
 });
 
