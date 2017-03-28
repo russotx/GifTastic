dccde414193a86b8c72f234ca039b7febc415160
russotx <russotx@gmail.com>
2017-03-27 23:51:42 -0500

    got gifs displayed and button to add buttons working

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -8,6 +8,9 @@
 var topics = ["rage","I'm out","not funny","middle finger","face palm",
 "are you serious","fail","party foul","eff my life","error"];
 var addATopic = "";
+var queryLimit = 10;
+var apiKey = 'dc6zaTOxFJmzC';
+
 
 /**************************
 *
@@ -16,7 +19,7 @@ var addATopic = "";
 ***************************/
 
 function buildButtons(newTopic){
-  console.log(newTopic);
+  //console.log(newTopic);
   if (newTopic != undefined) {
     topics.unshift(newTopic);
   }
@@ -29,9 +32,40 @@ function buildButtons(newTopic){
   }
 }
 
+function displayGifs(userParam) {
+  console.log("running get gifs");
+  var queryURL = 'http://api.giphy.com/v1/gifs/search?q='+userParam+'&api_key='+apiKey+'&limit='+queryLimit;
+  // query the api
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+  .done(function(response) {
+      // api returns an object with an array of objects
+      var gifResults = response.data;
+      console.log("the url sent: "+queryURL);
+      console.log("actual response: "+response);
+      console.log("gifResults: "+gifResults);
+      console.log("length of data array: "+response.data.length);
 
+      for (var i=0; i < gifResults.length; i++) {
+        var gifDiv = $('<div>');
+        var imageRating = $('<p>').text("Rating: "+gifResults[i].rating);
+        var newImage = $('<img>');
+        newImage.addClass('gif posted-gifs');
+        newImage.attr('src',gifResults[i].images.fixed_height_still.url);
+        newImage.attr('value',gifResults[i].id);
+        newImage.attr('data-still',gifResults[i].images.fixed_height_still.url);
+        newImage.attr('data-animate',gifResults[i].images.fixed_height.url);
+        newImage.attr('data-state','still');
+        gifDiv.append(newImage);
+        gifDiv.prepend(imageRating);
+        $('#gif-display').append(gifDiv);
+      }
 
+    });
 
+}
 
 /***************************
 *
@@ -40,12 +74,29 @@ function buildButtons(newTopic){
 ****************************/
 
 $(document).ready(function(){
+  // generate the initial buttons
   buildButtons();
-  $('#add-gif-btn').on("click",function(){
-    addATopic = $('#add-gif-form').val().trim();
-    buildButtons(addATopic);
+  // add user generated buttons
+
+  // get clicks on gif search buttons and display the gifs
+  $("#gif-buttons").on("click",'.gif-button',function(){
+    console.log("click on gif button returns- "+$(this));
+    $('#gif-display').html("");
+    var gifType = $(this).html();
+    console.log("the gif type on the button is: "+gifType);
+    var queryParam = gifType.split(' ').join('+');
+    console.log("gif type without spaces: "+queryParam);
+    displayGifs(queryParam);
   });
 
+  // Button to let user add their own gif search button
+  $('#add-gif-btn').on("click",function(){
+    console.log("just clicked submit button");
+    event.preventDefault();
+    addATopic = $('#add-gif-form').val().trim();
+    buildButtons(addATopic);
+    $('#add-gif-form').val("");
+  });
 
 
 
